d70a582ae1c2565fd543555757b8d462a8bcb576
Darshan Devani <ddevani@logilite.com>
2021-09-22 12:22:36 +0530

    Update in name coversion

--- java-practice-program/src/com/logilite/array/Array_Simple_Demo.java
+++ java-practice-program/src/com/logilite/array/Array_Simple_Demo.java
@@ -13,19 +13,19 @@ public class Array_Simple_Demo {
 
 	public static void main(String[] args) {
 
-		String[] city = { "Surat", "Ahemdabad", "Bangalore", "Pune", "Delhi" };
-		int[] number = new int[] { 100, 60, 80, 55, 93 };
+		String[] citys = { "Surat", "Ahemdabad", "Bangalore", "Pune", "Delhi" };
+		int[] populations = new int[] { 77, 80, 123, 68, 300 };
 
 		int i = 0;
-		while (i < city.length) {
-			String name = city[i];
+		while (i < citys.length) {
+			String name = citys[i];
 			System.out.println("City " + (i + 1) + " :- " + name);
 			i++;
 		}
 		System.out.println("\n");
-		for (int j = 0; j < number.length; j++) {
-			int num = number[j];
-			System.out.println("Number " + (j + 1) + " :- " + num);
+		for (int j = 0; j < populations.length; j++) {
+			int num = populations[j];
+			System.out.println("Population " + (j + 1) + " :- " + num+" lakhs");
 		}
 	}
 
