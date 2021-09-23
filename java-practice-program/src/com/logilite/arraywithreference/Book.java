f43bb9815150e1cb99c2e303d08ebe4d9f2cd56a
Darshan Devani <ddevani@logilite.com>
2021-09-23 09:40:33 +0530

    Array with Reference assignment

---
+++ java-practice-program/src/com/logilite/arraywithreference/Book.java
@@ -0,0 +1,32 @@
+package com.logilite.arraywithreference;
+
+public class Book {
+	public String title;
+
+	
+
+	public  Book(String title) {
+		this.title = title;
+	}
+
+	public void Book() {
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void getTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+		
+	}
+	public String toString() {
+		return "Book [title=" + title + "]";
+
+	}
+
+}
---
+++ java-practice-program/src/com/logilite/arraywithreference/Instructor.java
@@ -0,0 +1,60 @@
+package com.logilite.arraywithreference;
+
+public class Instructor {
+	public long id;
+	public String name;
+	public String title;
+	public String department;
+	public Book[] books;
+
+	public Instructor(long id, String name, String title, String department, Book[] books) {
+		this(id, name, title, department);
+		this.books = books;
+	}
+
+	public Instructor(long id, String name, String title, String department) {
+		this(id, name, title);
+		this.department = department;
+	}
+
+	public Instructor(long id, String name, String title) {
+		this(id, name);
+		this.title = title;
+	}
+
+	public Instructor(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getMostRecentBookTitle() {
+		return books[books.length - 1].getTitle();
+
+	}
+
+	public Book updateBook(int index, String title) {
+		Book oldBook = new Book(books[index].getTitle());
+		books[index].setTitle(title);
+		return oldBook;
+	}
+
+	public Book updateBook(int index, Book book) {
+		Book oldBook = books[index];
+		books[index] = book;
+		return oldBook;
+	}
+
+	public static void main(String[] args) {
+		Book book1 = new Book("C Language");
+		Book book2 = new Book("C++ Language");
+		Book book3 = new Book("java Language");
+
+		Instructor instructor = new Instructor(101, "John", "Assistant Professor", "Computer Science",
+				new Book[] { book1, book2, book3 });
+		System.out.println("Most Recent book :- " + instructor.getMostRecentBookTitle());
+		System.out.println("old book title: " + instructor.updateBook(1, "Advance Java").getTitle());
+
+		Book book4 = new Book("");
+		System.out.println("update book title: " + instructor.updateBook(1, book4).getTitle());
+	}
+}
