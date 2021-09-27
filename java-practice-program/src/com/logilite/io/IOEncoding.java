92c8df4846869182fecb23907d7593a4403a98fb
Darshan Devani <ddevani@logilite.com>
2021-09-27 13:53:33 +0530

    IO Encoding Demo

---
+++ java-practice-program/src/com/logilite/io/IOEncoding.java
@@ -0,0 +1,34 @@
+package com.logilite.io;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+public class IOEncoding {
+	
+	public static void applyEncoding() {
+		System.out.println("\nInside applyEncoding ...");
+		
+		//Show Eclipse property		
+		printEncodingDetails("DarshanDevani");//String
+		printEncodingDetails("€"); // Euro
+		printEncodingDetails("\u1F602"); // Non-BMP Unicode Code Point ~ Emoji     
+	}	
+	private static void printEncodingDetails(String symbol) {
+		System.out.println("\nSymbol: " + symbol);
+		try {
+			System.out.println("ASCII: " + Arrays.toString(symbol.getBytes("US-ASCII")));
+			System.out.println("ISO-8859-1: " + Arrays.toString(symbol.getBytes("ISO-8859-1")));
+			System.out.println("UTF-8: " + Arrays.toString(symbol.getBytes("UTF-8")));
+			System.out.println("UTF-16: " + Arrays.toString(symbol.getBytes("UTF-16")));
+			System.out.println("UTF-16 BE: " + Arrays.toString(symbol.getBytes("UTF-16BE")));
+			System.out.println("UTF-16 LE: " + Arrays.toString(symbol.getBytes("UTF-16LE")));
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}		
+	}
+
+	public static void main(String[] args) {
+		applyEncoding();
+	}
+
+}
