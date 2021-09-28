4ca19bc91ad5574a1c3fa99da7ae622dbb1b1c66
Darshan Devani <ddevani@logilite.com>
2021-09-28 11:36:51 +0530

    Final HashMap Demo

--- java-practice-program/src/com/logilite/map/HashMap_Demo.java
+++ java-practice-program/src/com/logilite/map/HashMap_Demo.java
@@ -8,60 +8,60 @@ public class HashMap_Demo {
 	private static void hashMapDemo() {
 		System.out.println("\nInside hashMapDemo ...");
 		Map<String, Integer> map1 = new HashMap<>();
-		//add key and value in map
+		// add key and value in map
 		map1.put("Darshan", 21);
 		map1.put("Hardik", 28);
 		map1.put("Piyush", null);
-		map1.put("Chintan", 30);	
+		map1.put("Chintan", 30);
 		map1.put("Amit", 130);
-		//add  value overwrite in map
+		// add value overwrite in map
 		map1.put("Piyush", 26);
 		System.out.println(map1);
-		//contains check value available or not
+		// contains check value available or not
 		System.out.println("Contains Piyush? " + map1.containsKey("Piyush"));
 		System.out.println("Chintann's age: " + map1.get("Chintan"));
-		//Show data Iterating using keySet ...
+		// Show data Iterating using keySet ...
 		System.out.println("\nIterating using keySet ...");
 		Set<String> names = map1.keySet();
 		for (String name : names) {
 			System.out.println("Name: " + name + ", Age: " + map1.get(name));
 		}
-		//Show data Iterating using entrySet ...
+		// Show data Iterating using entrySet ...
 		System.out.println("\nIterating using entrySet ...");
 		Set<Map.Entry<String, Integer>> mappings = map1.entrySet();
 		for (Map.Entry<String, Integer> mapping : mappings) {
 			System.out.println("Name: " + mapping.getKey() + ", Age: " + mapping.getValue());
 		}
-		//remove data using key and print
+		// remove data using key and print
 		names.remove("Amit");
 		System.out.println(map1);
-		
-		//Add new object value in the map
+
+		// Add new object value in the map
 		Map<String, Map<String, Object>> userProfile = new HashMap<>();
-		
+
 		Map<String, Object> profile = new HashMap<>();
 		profile.put("age", 25);
 		profile.put("dept", "CS");
 		profile.put("city", "New York");
-		
+
 		userProfile.put("Piyush", profile);
-		
+
 		profile = new HashMap<>();
 		profile.put("age", 29);
 		profile.put("dept", "CS");
 		profile.put("city", "New York");
-		
+
 		userProfile.put("Hardik", profile);
-		//print user profiles
+		// print user profiles
 		System.out.println("userProfile: " + userProfile);
 		// print age on the name or key
 		Map<String, Object> profile1 = userProfile.get("Piyush");
 		int age = (Integer) profile1.get("age");
 		System.out.println("Age: " + age);
-		
+
 	}
-		
+
 	public static void main(String[] args) {
-		hashMapDemo();		
-	}	
+		hashMapDemo();
+	}
 }
